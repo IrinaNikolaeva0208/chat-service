d054d064082f95c5fe1713345cfb59416360851e
Irina Nikolaeva <irina2002.nikolaeva@yandex.ru>
2024-01-30 10:50:50 +0300

    feat: add exception filter

--- server/src/chat/chat.gateway.ts
+++ server/src/chat/chat.gateway.ts
@@ -7,8 +7,10 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { MessageDto } from './dto/message.dto';
-import { Header } from '@nestjs/common';
+import { Header, UseFilters } from '@nestjs/common';
+import { AllExceptionsFilter } from './filters/ws.filter';
 
+@UseFilters(AllExceptionsFilter)
 @WebSocketGateway({ cors: '*:*' })
 export class ChatGateway {
   @WebSocketServer()
