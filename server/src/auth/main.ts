c30882bbd46f6a2f45e07ed21d122f227f0e4af2
Irina Nikolaeva <irina2002.nikolaeva@yandex.ru>
2024-02-13 21:33:04 +0300

    fix: rewrite the code of the service to grpc

--- server/src/auth/main.ts
+++ server/src/auth/main.ts
@@ -1,19 +1,24 @@
 import { NestFactory } from '@nestjs/core';
-import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 import { join } from 'path';
 import { AuthModule } from './auth.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AuthModule,
-    {
-      transport: Transport.GRPC,
-      options: {
-        package: 'hero',
-        protoPath: join(__dirname, 'hero/hero.proto'),
-      },
+  const app = await NestFactory.create(AuthModule);
+  app.connectMicroservice({
+    transport: Transport.GRPC,
+    options: {
+      package: 'auth',
+      protoPath: join(__dirname, 'proto/app.proto'),
     },
-  );
+  });
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('AUTH_PORT');
+
+  app.startAllMicroservices();
+  await app.listen(port);
 }
 
 bootstrap();
