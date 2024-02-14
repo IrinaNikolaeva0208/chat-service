cdd19baa219b1f32a5b4dda61617a1064a9f1d1b
Irina Nikolaeva <irina2002.nikolaeva@yandex.ru>
2024-02-14 12:45:56 +0300

    fix: fix proto bug

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,35 +1,29 @@
 import { Controller } from '@nestjs/common';
-import { MessagePattern, Payload } from '@nestjs/microservices';
+import { GrpcMethod } from '@nestjs/microservices';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @MessagePattern('createAuth')
-  create(@Payload() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
+  @GrpcMethod('AuthService', 'Register')
+  register(authDto: AuthDto) {
+    return this.authService.create(authDto);
   }
 
-  @MessagePattern('findAllAuth')
-  findAll() {
-    return this.authService.findAll();
+  @GrpcMethod('AuthService', 'LogIn')
+  login(authDto: AuthDto) {
+    return this.authService.createToken(authDto);
   }
 
-  @MessagePattern('findOneAuth')
-  findOne(@Payload() id: number) {
-    return this.authService.findOne(id);
+  @GrpcMethod('AuthService', 'LogOut')
+  logout() {
+    return this.authService.removeToken();
   }
 
-  @MessagePattern('updateAuth')
-  update(@Payload() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(updateAuthDto.id, updateAuthDto);
-  }
-
-  @MessagePattern('removeAuth')
-  remove(@Payload() id: number) {
-    return this.authService.remove(id);
+  @GrpcMethod('AuthService', 'Refresh')
+  refreshToken() {
+    return this.authService.refreshToken();
   }
 }
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,26 +1,19 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { AuthDto } from './dto/auth.dto';
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
+  create(authDto: AuthDto) {
+    return { id: 'd', username: authDto.username };
   }
 
-  findAll() {
-    return `This action returns all auth`;
+  createToken(authDto: AuthDto) {
+    return { result: `This action returns all auth` };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
+  removeToken() {
+    return `This action returns auth`;
   }
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
+  refreshToken() {}
 }
