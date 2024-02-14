ca5d251a3f792388b577b66c24d689ed9635077e
Irina Nikolaeva <irina2002.nikolaeva@yandex.ru>
2024-02-14 12:26:00 +0300

    fix: fix bug connected with protocol

--- server/src/auth/main.ts
+++ server/src/auth/main.ts
@@ -1,24 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { Transport } from '@nestjs/microservices';
-import { ConfigService } from '@nestjs/config';
 import { join } from 'path';
 import { AuthModule } from './auth.module';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 async function bootstrap() {
-  const app = await NestFactory.create(AuthModule);
-  app.connectMicroservice({
+  const app = await NestFactory.createMicroservice(AuthModule, {
     transport: Transport.GRPC,
     options: {
+      url: `auth:${process.env.AUTH_PORT}`,
       package: 'auth',
-      protoPath: join(__dirname, 'proto/app.proto'),
+      protoPath: join(__dirname, 'auth/proto/app.proto'),
     },
   });
 
-  const configService = app.get(ConfigService);
-  const port = configService.get<number>('AUTH_PORT');
-
-  app.startAllMicroservices();
-  await app.listen(port);
+  await app.listen();
 }
 
 bootstrap();
